8f848168d70b89e3fd093f259bb65922ee0d708b
Felipe Guizar Diaz <felipegaiacharly@gmail.com>
2019-02-23 13:25:30 -0600

    Finish stable version

--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
-import vue from 'vue'
-import { createRenderer } from 'vue-server-renderer'
-import hypernova, { serialize, load } from 'hypernova'
+import vue from 'vue';
+import { createRenderer } from 'vue-server-renderer';
+import hypernova, { serialize, load } from 'hypernova';
 
-export const Vue = vue
+export const Vue = vue;
 
 export const renderVue = (name, Component) => hypernova({
   server() {
     return async (propsData) => {
       const vm = new Component({
-        propsData
-      })
+        propsData,
+      });
 
       const renderer = createRenderer();
 
       const contents = await renderer.renderToString(vm);
 
-      return serialize(name, contents, propsData)
-    }
+      return serialize(name, contents, propsData);
+    };
   },
 
   client() {
     const payloads = load(name);
-    console.log(payloads)
     if (payloads) {
       payloads.forEach((payload) => {
         const { node, data: propsData } = payload;
-        
-        const vm = new Component({
-          propsData
-        })
 
-        vm.$mount(node.children[0])
+        const vm = new Component({
+          propsData,
+        });
+
+        vm.$mount(node.children[0]);
       });
     }
 
     return Component;
   },
-})
+});
